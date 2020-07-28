d482350c525480601885336e9af8b284455de78d
Ivan Kotlaja <encore.ivan@gmail.com>
2020-07-28 18:40:40 +0200

    Add middleware tests

--- back/test/api.test.js
+++ back/test/api.test.js
@@ -41,56 +41,58 @@ it('should fetch all users', () => {
     });
 });
 
-it('should fail if user does not follow logged user', () => {
-    jest.doMock('../db/index', () => setup())
-    const controller = require("../api/controllers/users.controller");
-    const user = { name: 'user1', id: 1 }
-    const params = { userId: 2 }
-    const resMock = {
-        json: jest.fn()
-    };
-
-    return controller.isFollowing({ user, params }, resMock).then(_ => {
-        expect(resMock.json.mock.calls.length).toBe(1);
-        expect(resMock.json.mock.calls[0][0]).toStrictEqual({ "isFollowing": false });
+describe('follower checks', () => {
+    it('should fail if user does not follow logged user', () => {
+        jest.doMock('../db/index', () => setup())
+        const controller = require("../api/controllers/users.controller");
+        const user = { name: 'user1', id: 1 }
+        const params = { userId: 2 }
+        const resMock = {
+            json: jest.fn()
+        };
+    
+        return controller.isFollowing({ user, params }, resMock).then(_ => {
+            expect(resMock.json.mock.calls.length).toBe(1);
+            expect(resMock.json.mock.calls[0][0]).toStrictEqual({ "isFollowing": false });
+        });
     });
-});
-
-it('should fail if user does not exist', () => {
-    jest.doMock('../db/index', () => setup({
-        findByPk: (userId) => null
-    }))
-    const controller = require("../api/controllers/users.controller");
-
-    const user = { name: 'user1', id: 1 }
-    const params = { userId: 1 }
-    const resMock = {
-        json: jest.fn(),
-        status: jest.fn()
-    };
-
-    return controller.isFollowing({ user, params }, resMock).then(_ => {
-        expect(resMock.json.mock.calls.length).toBe(1);
-        expect(resMock.status.mock.calls.length).toBe(1);
-        expect(resMock.json.mock.calls[0][0]).toStrictEqual({ "error": "No user with that id" });
+    
+    it('should fail if user does not exist', () => {
+        jest.doMock('../db/index', () => setup({
+            findByPk: (userId) => null
+        }))
+        const controller = require("../api/controllers/users.controller");
+    
+        const user = { name: 'user1', id: 1 }
+        const params = { userId: 1 }
+        const resMock = {
+            json: jest.fn(),
+            status: jest.fn()
+        };
+    
+        return controller.isFollowing({ user, params }, resMock).then(_ => {
+            expect(resMock.json.mock.calls.length).toBe(1);
+            expect(resMock.status.mock.calls.length).toBe(1);
+            expect(resMock.json.mock.calls[0][0]).toStrictEqual({ "error": "No user with that id" });
+        });
     });
-});
-
-it('should fail if user followers themselves', () => {
-    jest.doMock('../db/index', () => setup())
-    const controller = require("../api/controllers/users.controller");
-
-    const user = { name: 'user1', id: 1 }
-    const params = { userId: 1 }
-    const resMock = {
-        json: jest.fn(),
-        status: jest.fn()
-    };
-
-    return controller.isFollowing({ user, params }, resMock).then(_ => {
-        expect(resMock.json.mock.calls.length).toBe(1);
-        expect(resMock.status.mock.calls.length).toBe(1);
-        expect(resMock.json.mock.calls[0][0]).toStrictEqual({ "message": "You cannot follow yourself" });
-        expect(resMock.status.mock.calls[0][0]).toStrictEqual(409);
+    
+    it('should fail if user followers themselves', () => {
+        jest.doMock('../db/index', () => setup())
+        const controller = require("../api/controllers/users.controller");
+    
+        const user = { name: 'user1', id: 1 }
+        const params = { userId: 1 }
+        const resMock = {
+            json: jest.fn(),
+            status: jest.fn()
+        };
+    
+        return controller.isFollowing({ user, params }, resMock).then(_ => {
+            expect(resMock.json.mock.calls.length).toBe(1);
+            expect(resMock.status.mock.calls.length).toBe(1);
+            expect(resMock.json.mock.calls[0][0]).toStrictEqual({ "message": "You cannot follow yourself" });
+            expect(resMock.status.mock.calls[0][0]).toStrictEqual(409);
+        });
     });
-});
+})
